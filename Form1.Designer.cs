7b3f795b6ccdaeec4ea2ff13196534e4556510b2
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-04-19 18:24:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace dialog_classes
+{
+    partial class Text_edit
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.strip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dialog = new System.Windows.Forms.OpenFileDialog();
+            this.filedialog = new System.Windows.Forms.SaveFileDialog();
+            this.box = new System.Windows.Forms.TextBox();
+            this.strip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // strip
+            // 
+            this.strip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.strip.Location = new System.Drawing.Point(0, 0);
+            this.strip.Name = "strip";
+            this.strip.Size = new System.Drawing.Size(800, 24);
+            this.strip.TabIndex = 0;
+            this.strip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openingToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "файл";
+            // 
+            // openingToolStripMenuItem
+            // 
+            this.openingToolStripMenuItem.Name = "openingToolStripMenuItem";
+            this.openingToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openingToolStripMenuItem.Text = "открыть";
+            this.openingToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "сохранить как";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // dialog
+            // 
+            this.dialog.FileName = "openFileDialog1";
+            // 
+            // box
+            // 
+            this.box.Location = new System.Drawing.Point(12, 27);
+            this.box.Multiline = true;
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(766, 366);
+            this.box.TabIndex = 1;
+            // 
+            // Text_edit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.strip);
+            this.MainMenuStrip = this.strip;
+            this.Name = "Text_edit";
+            this.Text = "Текстовой редактор";
+            this.Load += new System.EventHandler(this.Text_edit_Load);
+            this.strip.ResumeLayout(false);
+            this.strip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip strip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dialog;
+        private System.Windows.Forms.SaveFileDialog filedialog;
+        private System.Windows.Forms.TextBox box;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dialog_classes
+{
+    public partial class Text_edit : Form
+    {
+        public Text_edit()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dialog.ShowDialog();
+            if (dialog.FileName == String.Empty) return;
+            try
+            {
+                var читатель = new System.IO.StreamReader(dialog.FileName, Encoding.GetEncoding(1251));
+                box.Text = читатель.ReadToEnd();
+                читатель.Close();
+            }
+            catch (System.IO.FileNotFoundException ситуация)
+            {
+                MessageBox.Show(ситуация.Message + "\nошибка", "ошибка поиска", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception ситуация)
+            {
+                MessageBox.Show(ситуация.Message, "ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            filedialog.FileName = dialog.FileName;
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var писатель = new System.IO.StreamWriter(filedialog.FileName, false, System.Text.Encoding.GetEncoding(1251));
+                    писатель.Write(box.Text);
+                    писатель.Close();
+                }
+                catch (Exception ситуация)
+                {
+                    MessageBox.Show(ситуация.Message, "ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Text_edit_Load(object sender, EventArgs e)
+        {
+            box.Clear();
+            dialog.FileName = @"data\Text2.txt";
+            dialog.Filter = "текстовые файлы (*.txt)|*.txt|all files (*.*)|*.*";
+            filedialog.Filter = "текстовые файлы (*.txt)|*.txt|all files (*.*)|*.*";
+        }
+    }
+}
